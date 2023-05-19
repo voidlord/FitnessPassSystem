9ece130c030d2367dafbf864bf47ecf9135cf7c2
void <lehelkov35@gmail.com>
2023-05-19 21:59:00 +0300

    Recreate project from scratch

---
+++ FitnessPass.Model/PassType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessPassApp.Data
+{
+    public class PassType
+    {
+        [Key]
+        public int PassId { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int DaysValidFor { get; set; }
+        public int EntriesValidFor { get; set; }
+        public bool Deleted { get; set; }
+        public int GymId { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public int MaxDailyUse { get; set; }
+    }
+}
