c011dd9666c114f664b5ea80bebd83f1249c2075
kacsorobert <kacsorobert01@gmail.com>
2023-05-23 19:53:00 +0300

    user service

--- FitnessPass.Service/UserService.cs
+++ FitnessPass.Service/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using FitnessPass.DB;
 using FitnessPass.Model;
+using FitnessPassApp.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,41 @@ namespace FitnessPass.Service {
 
         public User Login(string email, string password) {
             return appDbContext.User.Where(x => x.Email == email && x.Password == password).ToList().First();
+        }
+
+        public List<User> GetUsers() 
+        {
+            return appDbContext.User.ToList();
+        }
+
+        public void AddUser(User user)
+        {
+            user.CreatedOn = DateTime.Now;
+            user.IsDeleted = false;
+            appDbContext.User.Add(user);
+            appDbContext.SaveChanges();
+        }
+
+        public List<User> SearchPassTypeByName(string name)
+        {
+            return appDbContext.User.Where(x => x.Name.Contains(name)).ToList();
+        }
+
+        public User GetUserById(int id)
+        {
+            return appDbContext.User.Find(id) ?? new User();
+        }
+
+        public void UpdateUser(User user) 
+        {
+            appDbContext.User.Update(user);
+            appDbContext.SaveChanges();
+        }
+
+        public void DeleteUserById(int id)
+        {
+            appDbContext.User.Find(id).IsDeleted = true;
+            appDbContext.SaveChanges();
         }
     }
 }
