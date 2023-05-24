87d29a366a7aef6dbe8da50133b43a5335e1c010
void <lehelkov35@gmail.com>
2023-05-24 15:55:51 +0300

    Create ClientPassService

---
+++ FitnessPass.Service/ClientPassService.cs
@@ -0,0 +1,21 @@
+﻿using FitnessPass.DB;
+using FitnessPass.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessPass.Service {
+    public class ClientPassService {
+        private AppDbContext appDbContext;
+
+        public ClientPassService(AppDbContext appDbContext) {
+            this.appDbContext = appDbContext;
+        }
+
+        public List<ClientPass> GetPassesByClientId(int clientId) {
+            return appDbContext.ClientPass.Where(x => x.ClientId == clientId).ToList();
+        }
+    }
+}
